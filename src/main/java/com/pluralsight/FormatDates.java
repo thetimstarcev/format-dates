8af5e6924edf6f19aef4c41392710df3b2681f26
thetimstarcev <132023910+thetimstarcev@users.noreply.github.com>
2026-04-22 10:24:14 -0400

    Completed exercise 2 (Workbook 3a). Got the current date and time and displayed that information in the requested formats

--- src/main/java/com/pluralsight/FormatDates.java
+++ src/main/java/com/pluralsight/FormatDates.java
@@ -1,4 +1,36 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+
 public class FormatDates {
+    public static void main(String[] args) {
+        LocalDateTime today = LocalDateTime.now();
+
+        //Formatter 1
+        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+
+        //Formatter 2
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        //Formatter 2
+        DateTimeFormatter formatter3 = DateTimeFormatter.ofPattern("EEEE dd, yyyy");
+
+        //Formatter 4
+        ZonedDateTime gmt = ZonedDateTime.now();
+        DateTimeFormatter formatter4 = DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy HH:mm");
+
+        //Formatter 5
+        DateTimeFormatter formatter5 = DateTimeFormatter.ofPattern("h:mm 'on'  dd-MMM-yyyy");
+
+        //Outputs
+        System.out.println(today.format(formatter1));
+        System.out.println(today.format(formatter2));
+        System.out.println(today.format(formatter3));
+        System.out.println(today.format(formatter4));
+        System.out.println(today.format(formatter5));
+    }
 }
